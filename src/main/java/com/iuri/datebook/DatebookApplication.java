14fa5b9d527437909c8ac53a469d0bf806a0fba0
iuri <bentoiuri@gmail.com>
2025-01-13 09:07:37 -0300

    commit inicial

---
+++ src/main/java/com/iuri/datebook/DatebookApplication.java
@@ -0,0 +1,13 @@
+package com.iuri.datebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatebookApplication.class, args);
+	}
+
+}
