29979ca3c81760fa26532b0ca032de0d40804578
iuri <bentoiuri@gmail.com>
2025-02-03 17:09:20 -0300

    adiciona classe User e JwtUtil

---
+++ src/main/java/com/iuri/datebook/model/User.java
@@ -0,0 +1,23 @@
+package com.iuri.datebook.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "user")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+}
